c7926ec3030a9f55538a7debd6cd27aee8968f90
Sean Current <seancurrent527@email.arizona.edu>
2021-04-05 16:15:18 -0400

    Fixed loss function to use L1 norm and scale to proportions.

--- models/fair_losses.py
+++ models/fair_losses.py
@@ -6,6 +6,7 @@ def fair_link_loss(attributes, links):
     partial = tf.reduce_sum(links, axis = -1)
     #partial is (batch, filters, nodes)
     f = tf.squeeze(tf.matmul(tf.transpose(attributes, (0, 1, 3, 2)), partial[..., None]))
+    f = f / tf.reduce_sum(f, axis = -1)
     #f is (batch, filters, attributes)
     e = tf.ones_like(f)
-    return tf.losses.kld(f / tf.norm(f), e / tf.norm(e))
+    return tf.losses.kld(f / tf.norm(f, ord=1), e / tf.norm(e, ord=2))
