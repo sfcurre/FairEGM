337cea325ccb49cf5eb5aa61aef791684f749b4c
Sean Current <seancurrent527@email.arizona.edu>
2021-04-05 18:03:50 -0400

    Fixed a typo for norm scaling the fair loss.

--- models/fair_losses.py
+++ models/fair_losses.py
@@ -6,7 +6,7 @@ def fair_link_loss(attributes, links):
     partial = tf.reduce_sum(links, axis = -1)
     #partial is (batch, filters, nodes)
     f = tf.squeeze(tf.matmul(tf.transpose(attributes, (0, 1, 3, 2)), partial[..., None]))
-    f = f / tf.reduce_sum(f, axis = -1)
+    f = f / tf.reduce_sum(attributes, axis = 2)
     #f is (batch, filters, attributes)
     e = tf.ones_like(f)
     return tf.losses.kld(f / tf.norm(f, ord=1), e / tf.norm(e, ord=2))
