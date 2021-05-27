de1bd8052504fa3837699c18eeef36594a55f66a
Sean Current <sfcurre@owens-rw02.ten.osc.edu>
2021-05-27 10:48:10 -0400

    Results from OSC runs.

--- slurm-main.sh
+++ slurm-main.sh
@@ -16,24 +16,26 @@ set -e
 source /usr/local/python/3.6-conda5.2/etc/profile.d/conda.sh
 conda activate deepml4
 
-python main.py --dataset citeseer -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python main.py --dataset cora -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python main.py --dataset facebook -d 128 -lr 0.001 -e 300 -k 20 -f 5
+cd ~/FairGraphs
+
+#python main.py --dataset citeseer -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python main.py --dataset cora -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python main.py --dataset facebook -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
 #python main.py --dataset credit -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python main.py --dataset pubmed -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python main.py --dataset pubmed -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
-python cfo.py --dataset citeseer -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python cfo.py --dataset cora -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python cfo.py --dataset facebook -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python cfo.py --dataset citeseer -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python cfo.py --dataset cora -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python cfo.py --dataset facebook -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
 #python cfo.py --dataset credit -d 128 -lr 0.001 -e 300 -k 20 -f 5
-python cfo.py --dataset pubmed -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python cfo.py --dataset pubmed -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
-python gfo.py --dataset citeseer -d 128 -e 300 -k 20 -f 5
-python gfo.py --dataset cora -d 128 -e 300 -k 20 -f 5
-python gfo.py --dataset facebook -d 128 -e 300 -k 20 -f 5
+#python gfo.py --dataset citeseer -d 128 -e 300 -k 20 -f 5
+#python gfo.py --dataset cora -d 128 -e 300 -k 20 -f 5
+#python gfo.py --dataset facebook -d 128 -e 300 -k 20 -f 5
 
-python gfo.py --dataset pubmed -d 128 -e 300 -k 20 -f 5
+#python gfo.py --dataset pubmed -d 128 -e 300 -k 20 -f 5
 
 python baselines.py
