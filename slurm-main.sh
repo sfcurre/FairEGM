5c3ecc803fe510d721465d1102ffe07823633de4
Sean Current <seancurrent527@email.arizona.edu>
2021-06-04 17:49:42 -0400

    Remove runs on credit dataset.

--- slurm-main.sh
+++ slurm-main.sh
@@ -22,7 +22,7 @@ cd ~/FairGraphs
 #python main.py --dataset cora -d 128 -lr 0.001 -e 300 -k 20 -f 5
 #python main.py --dataset facebook -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
-python main.py --dataset credit -d 128 -lr 0.001 -e 300 -k 20 -f 5
+#python main.py --dataset credit -d 128 -lr 0.001 -e 300 -k 20 -f 5
 #python main.py --dataset pubmed -d 128 -lr 0.001 -e 300 -k 20 -f 5
 
 #python cfo.py --dataset citeseer -d 128 -lr 0.001 -e 300 -k 20 -f 5
@@ -36,7 +36,7 @@ python main.py --dataset credit -d 128 -lr 0.001 -e 300 -k 20 -f 5
 #python gfo.py --dataset cora -d 128 -e 300 -k 20 -f 5
 #python gfo.py --dataset facebook -d 128 -e 300 -k 20 -f 5
 
-python gfo.py --dataset credit -d 128 -e 300 -k 20 -f 5
+#python gfo.py --dataset credit -d 128 -e 300 -k 20 -f 5
 #python gfo.py --dataset pubmed -d 128 -e 300 -k 20 -f 5
 
 python baselines.py
