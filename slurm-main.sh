b801b51269a530910df9352d869281ef487e015d
Sean Current <seancurrent527@email.arizona.edu>
2021-08-17 21:12:10 -0400

    Updated models to run for more epochs at a higher learning rate.

--- slurm-main.sh
+++ slurm-main.sh
@@ -18,24 +18,24 @@ conda activate deepml4
 
 cd ~/FairGraphs
 
-python main.py --dataset citeseer -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python main.py --dataset cora -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python main.py --dataset facebook -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python main.py --dataset german -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+python main.py --dataset citeseer -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
+python main.py --dataset cora -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
+python main.py --dataset facebook -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
+python main.py --dataset german -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
 
-python main.py --dataset bail -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+python main.py --dataset bail -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
 # python main.py --dataset credit -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python main.py --dataset pubmed -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+python main.py --dataset pubmed -d 128 -lr 0.1 -e 2000 -k 5 10 20 40 -f 5
 
-python cfo.py --dataset citeseer -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python cfo.py --dataset cora -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python cfo.py --dataset facebook -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
-python cfo.py --dataset german -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+# python cfo.py --dataset citeseer -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+# python cfo.py --dataset cora -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+# python cfo.py --dataset facebook -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
+# python cfo.py --dataset german -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
 
-python gfo.py --dataset citeseer -d 128 -e 100 -k 5 10 20 40 -f 5
-python gfo.py --dataset cora -d 128 -e 100 -k 5 10 20 40 -f 5
-python gfo.py --dataset facebook -d 128 -e 100 -k 5 10 20 40 -f 5
-python gfo.py --dataset german -d 128 -e 100 -k 5 10 20 40 -f 5
+# python gfo.py --dataset citeseer -d 128 -e 100 -k 5 10 20 40 -f 5
+# python gfo.py --dataset cora -d 128 -e 100 -k 5 10 20 40 -f 5
+# python gfo.py --dataset facebook -d 128 -e 100 -k 5 10 20 40 -f 5
+# python gfo.py --dataset german -d 128 -e 100 -k 5 10 20 40 -f 5
 
 # python gfo.py --dataset bail -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
 # python gfo.py --dataset credit -d 128 -lr 0.001 -e 100 -k 5 10 20 40 -f 5
